a8d1eebf6167e9e2deef28e87023a70e8934226d
jaeha <jaeha23@naver.com>
2021-11-13 14:06:42 +0900

    create Screen

---
+++ app/components/Screen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet, SafeAreaView, StatusBar} from 'react-native';
+
+function Screen({children, style}) {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor={'transparent'} translucent={true} />
+      <SafeAreaView style={{...styles.screen, ...style}}>{children}</SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: StatusBar.currentHeight,
+    flex: 1,
+  },
+});
+
+export default Screen;
