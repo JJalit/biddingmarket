8eaf49a4713707eef19a199761414774ff2ecb42
jaeha <jaeha23@naver.com>
2021-11-22 23:26:10 +0900

    add Screen to Webview

--- app/screens/LandingScreen/index.js
+++ app/screens/LandingScreen/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import {WebView} from 'react-native-webview';
 
+import Screen from '../../components/Screen';
+
 const LandingScreen = () => {
-  return <WebView source={{uri: 'https://m.bidingmarket.com/'}} />;
+  return (
+    <Screen>
+      <WebView source={{uri: 'https://m.bidingmarket.com/'}} />
+    </Screen>
+  );
 };
 
 export default LandingScreen;
