dd4ef89cbc72d1fa042442e793a3b078b977325d
jaeha <jaeha23@naver.com>
2021-11-22 23:05:28 +0900

    react navigation bottom tabs

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import RegisterScreen from './app/screens/RegisterScreen';
 import InfoScreen from './app/screens/InfoScreen';
 import FindScreen from './app/screens/FindScreen';
+import LandingScreen from './app/screens/LandingScreen';
+import AppNavigator from './app/navigation/AppNavigator';
 
 const App = () => {
-  return <WelcomeScreen />;
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 };
 
 export default App;
---
+++ app/navigation/AppNavigator.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import LandingNavigator from './LandingNavigator';
+import NotificationNavigator from './NotificationNavigator';
+import UploadNavigator from './UploadNavigator';
+import FavoriteNavigator from './FavoriteNavigator';
+import AccountNavigator from './AccountNavigator';
+
+const Tab = createBottomTabNavigator();
+
+const AppNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#ae3ec9',
+        tabBarHideOnKeyboard: true,
+        tabBarShowLabel: false,
+      }}>
+      <Tab.Screen
+        name="홈"
+        component={LandingNavigator}
+        options={{
+          tabBarIcon: ({color, size, focused}) => <Icon name={focused ? 'home' : 'home-outline'} color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="알림"
+        component={NotificationNavigator}
+        options={{
+          tabBarIcon: ({color, size, focused}) => <Icon name={focused ? 'bell' : 'bell-outline'} color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="등록"
+        component={UploadNavigator}
+        options={{
+          tabBarIcon: ({color, size, focused}) => <Icon name={focused ? 'plus-box' : 'plus-box-outline'} color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="북마크"
+        component={FavoriteNavigator}
+        options={{
+          tabBarIcon: ({color, size, focused}) => <Icon name={focused ? 'star' : 'star-outline'} color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="설정"
+        component={AccountNavigator}
+        options={{
+          tabBarIcon: ({color, size, focused}) => <Icon name={focused ? 'account' : 'account-outline'} color={color} size={size} />,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default AppNavigator;
