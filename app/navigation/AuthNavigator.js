f06d4d47b31f8678500a051d2a28d6376e369613
jaeha <jaeha23@naver.com>
2021-12-12 15:45:36 +0900

    add AuthNavigator's Stack

--- app/navigation/AuthNavigator.js
+++ app/navigation/AuthNavigator.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import WelcomeScreen from '../screens/WelcomeScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import FindScreen from '../screens/FindScreen';
 
 const Stack = createStackNavigator();
 
 const AuthNavigator = () => (
-  <Stack.Navigator screenOptions={{ headerStyle: { shadowColor: 'transparent' } }}>
-    <Stack.Screen name="로그인" component={WelcomeScreen} />
+  <Stack.Navigator
+    screenOptions={{
+      headerBackTitleVisible: false,
+      headerTitle: '',
+      headerTintColor: '#7E7D7D',
+    }}>
+    <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    <Stack.Screen name="Register" component={RegisterScreen} />
+    <Stack.Screen name="Find" component={FindScreen} />
   </Stack.Navigator>
 );
 
